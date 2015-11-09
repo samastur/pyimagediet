6b3925972ab51785fbe74d3ec025c90daaf8a3f0
Marko Samastur <markos@gaivo.net>
2015-11-09 13:41:30 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+long_description = '''\
+pyimagediet is a Python wrapper around image optimisations tools used to
+reduce images size without loss of visual quality. It provides a uniform
+interface to tools, easy configuration and integration.
+
+It works on images in JPEG, GIF and PNG formats and will leave others
+unchanged.'''
+
+setup(
+    author="Marko Samastur",
+    author_email="markos@gaivo.net",
+    name='pyimagediet',
+    version='0.5',
+    description='Python wrapper around image optimisations tools',
+    long_description=long_description,
+    url='https://github.com/samastur/pyimagediet/',
+    platforms=['OS Independent'],
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'PyYAML>=3.11',
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False
+)
