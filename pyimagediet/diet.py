56237752e6b36dc9257193db842f753841bfe372
Marko Samastur <markos@gaivo.net>
2015-11-12 15:20:13 +0100

    Parse configuration and create pipelines
    parse_configuration function makes a copy of a configuration which:
    - fixes outputs so that processed file is the same as original input
      (in place)
    - pipelines now contain commands to run for each configured file type

--- pyimagediet/diet.py
+++ pyimagediet/diet.py
@@ -1,14 +1,17 @@
+import copy
 import magic
 import yaml
 
 
 def read_yaml_configuration(filename):
+    '''Parse configuration in YAML format into a Python dict'''
     with open(filename, 'r') as f:
         config = yaml.load(f.read())
     return config
 
 
 def determinetype(filename):
+    '''Determine the file type and return it.'''
     ftype = magic.from_file(filename, mime=True).decode('utf8')
     if ftype == 'text/plain':
         ftype = 'text'
@@ -17,3 +20,29 @@ def determinetype(filename):
     else:
         ftype = ftype.split('/')[1]
     return ftype
+
+
+def parse_configuration(config):
+    '''
+    Parse and fix configuration:
+        - processed file should end up being same as input
+        - pipelines should contain CLI commands to run
+    '''
+    new_config = copy.deepcopy(config)
+
+    # Always end up with input file. If app outputs to a different one,
+    # then replace old one with it
+    for prog in new_config['parameters']:
+        if '{output_file}' in new_config['parameters'][prog]:
+            new_config['parameters'][prog] = new_config['parameters'][prog] + " && mv '{output_file}' '{file}'"
+
+    # Build pipelines
+    for label, raw_pipeline in new_config['pipelines'].items():
+        commands = []
+        for app in raw_pipeline:
+            full_command = " ".join([new_config['commands'][app],
+                                     new_config['parameters'][app]])
+            commands.append(full_command)
+        new_config['pipelines'][label] = " && ".join(commands)
+
+    return new_config
--- tests/test_diet.py
+++ tests/test_diet.py
@@ -1,7 +1,34 @@
+import copy
 from os.path import abspath, dirname, join
+import pytest
+
 import pyimagediet.diet as diet
 
 TEST_DIR = abspath(dirname(__file__))
+TEST_CONFIG = {
+	'commands': {
+		'advpng': '/usr/local/bin/advpng',
+		'gifsicle': '/usr/local/bin/gifsicle',
+		'jpegoptim': '/usr/local/bin/jpegoptim',
+		'jpegtran': '/usr/local/bin/jpegtran',
+		'optipng': '/usr/local/bin/optipng',
+		'pngcrush': '/usr/local/bin/pngcrush'},
+	'parameters': {
+		'advpng': "-z4 '{file}s'",
+		'gifsicle': "-O2 '{file}' > '{output_file}'",
+		'jpegoptim': "-f --strip-all '{file}s'",
+		'jpegtran': "-copy none -progressive -optimize -outfile '{output_file}' '{file}'",
+		'optipng': "-force -o7 '{file}'",
+		'pngcrush': "-rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time '{file}' '{output_file}'"},
+	'pipelines': {
+		'gif': ['gifsicle'],
+		'jpeg': ['jpegtran'],
+		'png': ['optipng', 'advpng', 'pngcrush']}}
+
+
+@pytest.fixture
+def config_copy():
+	return copy.deepcopy(TEST_CONFIG)
 
 
 def test_read_yaml_configuration():
@@ -38,3 +65,36 @@ def test_determinetype_returns_svg_on_svg_images():
     f = join(TEST_DIR, 'test_files/nature.svg')
     ftype = diet.determinetype(f)
     assert ftype == 'svg'
+
+
+def test_parse_configuration_correctly_handles_output_file(config_copy):
+	corrected = {
+		'gifsicle': "-O2 '{file}' > '{output_file}' && mv '{output_file}' '{file}'",
+		'jpegtran': "-copy none -progressive -optimize -outfile '{output_file}' '{file}' && mv '{output_file}' '{file}'",
+		'pngcrush': "-rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time '{file}' '{output_file}' && mv '{output_file}' '{file}'"
+	}
+	config = diet.parse_configuration(config_copy)
+	for prog, params in config['parameters'].items():
+		if '{output_file}' in params:
+			assert params == corrected[prog]
+
+
+def test_parse_configuration_correctly_builds_pipelines(config_copy):
+	config = diet.parse_configuration(config_copy)
+	pipelines = {
+		'gif': (
+			"/usr/local/bin/gifsicle -O2 '{file}' > '{output_file}' "
+			"&& mv '{output_file}' '{file}'"),
+		'jpeg': (
+			"/usr/local/bin/jpegtran -copy none -progressive -optimize -outfile "
+			"'{output_file}' '{file}' && mv '{output_file}' '{file}'"),
+		'png': (
+			"/usr/local/bin/optipng -force -o7 '{file}' "
+			"&& /usr/local/bin/advpng -z4 '{file}s' "
+			"&& /usr/local/bin/pngcrush -rem gAMA -rem alla -rem cHRM -rem iCCP "
+			"-rem sRGB -rem time '{file}' '{output_file}' && mv '{output_file}' "
+			"'{file}'")
+	}
+	for img_type, pipeline in config['pipelines'].items():
+		assert pipeline == pipelines[img_type]
+
