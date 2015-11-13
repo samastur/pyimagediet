998d2ddf3fc8a1fa03daacd79afc9208bab3e0c4
Marko Samastur <markos@gaivo.net>
2015-11-13 12:23:28 +0100

    Use tox as test runner in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
 
 long_description = '''\
 pyimagediet is a Python wrapper around image optimisations tools used to
@@ -38,5 +60,7 @@ setup(
     ],
     include_package_data=True,
     packages=['pyimagediet'],
+    tests_require=['tox'],
+    cmdclass = {'test': Tox},
     zip_safe=False
 )
