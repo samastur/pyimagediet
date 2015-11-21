9dcee10c59cd32745b64ae827d267b0464e1040d
Marko Samastur <markos@gaivo.net>
2015-11-21 17:50:40 +0100

    Upped package version to 0.9.1
    ...because diet function now returns a different status (was
    file changed or not).

--- pyimagediet/__init__.py
+++ pyimagediet/__init__.py
@@ -8,7 +8,7 @@ from  .process import (
 
 __author__ = ("Marko Samastur <markos@gaivo.net>")
 
-__version__ = "0.9"
+__version__ = "0.9.1"
 
 __all__ = [
     NotFileDietException, ConfigurationErrorDietException,
